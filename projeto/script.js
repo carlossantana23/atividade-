4e677f5bda53028d9ee8930ce865f7fae3750995
carlos-rt23 <carlossantana23j@gmail.com>
2026-07-22 21:18:30 -0300

    Add files via upload

---
+++ projeto/script.js
@@ -0,0 +1,19 @@
+const modal = document.getElementById("btnModal")
+const descricao = document.getElementById("Descricao")
+const imagem = document.getElementById("imagemAnimal") 
+
+document.getElementById('btnCachorro').addEventListener('click') , () => {
+    imagem.src = "https://encrypted-tbn0.gstatic.com/imagem?"
+    
+});
+document.getElementById('btnCachorro').addEventListener('click') , () => {
+    imagem.src =
+
+})
+document.getElementById('btnCachorro').addEventListener('click') , () => {
+    imagem.s
+})
+document.getElementById('btnCachorro').addEventListener('click') , () => {
+
+})
+document.getElementById('btnCachorro').addEventListener('click') , () => {
